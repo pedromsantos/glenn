f787699651c7aa10ae2b70a453f670363c1f5db5
Pedro Santos <7371026-pedro_m_santos@users.noreply.gitlab.com>
2022-01-08 21:20:42 +0100

    Implement major and minor keys

---
+++ src/Domain/Key.ts
@@ -0,0 +1,97 @@
+import Pitch from './Pitch';
+
+export default class Key {
+  constructor(private root: Pitch, private accidentals: number) {}
+
+  public static readonly CMajor: Key = new Key(Pitch.C, 0);
+  public static readonly DFlatMajor: Key = new Key(Pitch.DFlat, -5);
+  public static readonly DMajor: Key = new Key(Pitch.D, 2);
+  public static readonly EFlatMajor: Key = new Key(Pitch.EFlat, -3);
+  public static readonly EMajor: Key = new Key(Pitch.E, 4);
+  public static readonly FMajor: Key = new Key(Pitch.F, -1);
+  public static readonly FSharpMajor: Key = new Key(Pitch.FSharp, 6);
+  public static readonly GFlatMajor: Key = new Key(Pitch.GFlat, -6);
+  public static readonly GMajor: Key = new Key(Pitch.G, 1);
+  public static readonly AFlatMajor: Key = new Key(Pitch.AFlat, -4);
+  public static readonly AMajor: Key = new Key(Pitch.A, 3);
+  public static readonly BFlatMajor: Key = new Key(Pitch.BFlat, -2);
+  public static readonly BMajor: Key = new Key(Pitch.B, 5);
+
+  public static readonly CMinor: Key = new Key(Pitch.C, -3);
+  public static readonly CSharpMinor: Key = new Key(Pitch.CSharp, 4);
+  public static readonly DMinor: Key = new Key(Pitch.D, -1);
+  public static readonly EFlatMinor: Key = new Key(Pitch.EFlat, -6);
+  public static readonly EMinor: Key = new Key(Pitch.E, 1);
+  public static readonly FMinor: Key = new Key(Pitch.F, -4);
+  public static readonly FSharpMinor: Key = new Key(Pitch.FSharp, 3);
+  public static readonly GMinor: Key = new Key(Pitch.G, -2);
+  public static readonly GSharpMinor: Key = new Key(Pitch.GSharp, 5);
+  public static readonly AMinor: Key = new Key(Pitch.A, 0);
+  public static readonly BFlatMinor: Key = new Key(Pitch.BFlat, -5);
+  public static readonly BMinor: Key = new Key(Pitch.B, 2);
+
+  private flatKey(fifths: Pitch[]): Pitch[] {
+    const toFlat = fifths.slice(this.accidentals);
+    const naturals = fifths.slice(0, this.accidentals);
+
+    return toFlat.map((p) => p.flat()).concat(naturals);
+  }
+
+  private sharpKey(fifths: Pitch[]): Pitch[] {
+    const toSharp = fifths.slice(0, this.accidentals);
+    const naturals = fifths.slice(this.accidentals);
+
+    return toSharp.map((p) => p.sharp()).concat(naturals);
+  }
+
+  private unsortedNotes() {
+    const fifths: Pitch[] = [Pitch.F, Pitch.C, Pitch.G, Pitch.D, Pitch.A, Pitch.E, Pitch.B];
+
+    if (this.accidentals == 0) return fifths;
+    if (this.accidentals < 0) return this.flatKey(fifths);
+    return this.sharpKey(fifths);
+  }
+
+  public notes(): Pitch[] {
+    const unorderedNotes = this.unsortedNotes().sort((p1, p2) =>
+      p1.NumericValue < p2.NumericValue ? -1 : 1
+    );
+
+    const rootIndex = unorderedNotes.indexOf(this.root);
+    const orderedNotes = unorderedNotes.slice(rootIndex).concat(unorderedNotes.slice(0, rootIndex));
+    return orderedNotes;
+  }
+
+  public static readonly majorKeys = [
+    Key.AMajor,
+    Key.AFlatMajor,
+    Key.BMajor,
+    Key.BFlatMajor,
+    Key.CMajor,
+    Key.DMajor,
+    Key.DFlatMajor,
+    Key.EMajor,
+    Key.EFlatMajor,
+    Key.FMajor,
+    Key.FSharpMajor,
+    Key.GMajor,
+    Key.GFlatMajor,
+  ];
+
+  public static readonly minorKeys = [
+    Key.AMinor,
+    Key.BMinor,
+    Key.BFlatMinor,
+    Key.CMinor,
+    Key.CSharpMinor,
+    Key.DMinor,
+    Key.EMinor,
+    Key.FMinor,
+    Key.FSharpMinor,
+    Key.GMinor,
+    Key.GSharpMinor,
+    Key.EFlatMinor,
+  ];
+
+  public static readonly keys = Key.majorKeys.concat(Key.minorKeys);
+}
---
+++ src/__test__/Domain/Key.test.ts
@@ -0,0 +1,88 @@
+import Pitch from '../../Domain/Pitch';
+import Interval from '../../Domain/Interval';
+import Key from '../../Domain/Key';
+import * as fc from 'fast-check';
+
+describe('Major keys', () => {
+  test('C should have notes C, D, E, F, G, A and B', () => {
+    expect(Key.CMajor.notes()).toStrictEqual([
+      Pitch.C,
+      Pitch.D,
+      Pitch.E,
+      Pitch.F,
+      Pitch.G,
+      Pitch.A,
+      Pitch.B,
+    ]);
+  });
+
+  test('F should have notes  F, G, A, Bb, C, D, E', () => {
+    expect(Key.FMajor.notes()).toStrictEqual([
+      Pitch.F,
+      Pitch.G,
+      Pitch.A,
+      Pitch.BFlat,
+      Pitch.C,
+      Pitch.D,
+      Pitch.E,
+    ]);
+  });
+
+  test('G should have notes  G, A, B, C, D, E, F#', () => {
+    expect(Key.GMajor.notes()).toStrictEqual([
+      Pitch.G,
+      Pitch.A,
+      Pitch.B,
+      Pitch.C,
+      Pitch.D,
+      Pitch.E,
+      Pitch.FSharp,
+    ]);
+  });
+});
+
+describe('properties', () => {
+  test('Major keys have formula W, W, H, W, W, W, H', () => {
+    const majorKeyDistances = [2, 2, 1, 2, 2, 2, 1];
+
+    fc.assert(
+      fc.property(fc.constantFrom(...Key.majorKeys), (key) => {
+        const keyNotes = key.notes().concat(key.notes()[0]);
+
+        const scaleNotePairs = keyNotes
+          .reduce((result, _value, index, array) => {
+            result.push(array.slice(index, index + 2));
+            return result;
+          }, [])
+          .slice(0, -1);
+
+        const distances = scaleNotePairs.map((p) => p[0].absoluteDistance(p[1]));
+
+        expect(distances).toStrictEqual(majorKeyDistances);
+      }),
+      { verbose: true }
+    );
+  });
+
+  test('Minor keys have formula W, H, W, W, H, W, W', () => {
+    const minorKeyDistances = [2, 1, 2, 2, 1, 2, 2];
+
+    fc.assert(
+      fc.property(fc.constantFrom(...Key.minorKeys), (key) => {
+        const keyNotes = key.notes().concat(key.notes()[0]);
+
+        const scaleNotePairs = keyNotes
+          .reduce((result, _value, index, array) => {
+            result.push(array.slice(index, index + 2));
+            return result;
+          }, [])
+          .slice(0, -1);
+
+        const distances = scaleNotePairs.map((p) => p[0].absoluteDistance(p[1]));
+
+        expect(distances).toStrictEqual(minorKeyDistances);
+      }),
+      { verbose: true }
+    );
+  });
+});
