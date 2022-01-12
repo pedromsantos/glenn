eb14f1ca7f717286f5f1afcc02aab019abeac63b
Pedro Santos <7371026-pedro_m_santos@users.noreply.gitlab.com>
2022-01-12 20:41:58 +0100

    Add jest config to package json

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   preset: 'ts-jest',
-  collectCoverage: true,
+  collectCoverage: false,
   testEnvironment: 'node',
   coverageReporters: ['clover', 'json', 'lcov', ['text', { skipFull: true }]],
-  coverageThreshold: {
-    global: {
-      branches: 85,
-      functions: 90,
-      lines: 90,
-      statements: -10,
-    },
-  },
 };
