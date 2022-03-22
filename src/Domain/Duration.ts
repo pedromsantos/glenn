95f73d4f35bc2acce204a90eb87ca786024b6eec
Pedro Santos <7371026-pedro_m_santos@users.noreply.gitlab.com>
2022-03-22 17:04:37 +0100

    Fix error starting app

--- src/Domain/Duration.ts
+++ src/Domain/Duration.ts
@@ -36,13 +36,15 @@ export class CompoundTimeSignature implements TimeSignature {
     return this.duration.value * 3;
   }
 
-  // toBeats(duration: Duration = this.duration): number {
-  //   return (duration.value * 3) / this.beatDuration / this.beats;
-  // }
+  toBeats(duration: Duration = this.duration): number {
+    return 0;
+    //return (duration.value * 3) / this.beatDuration / this.beats;
+  }
 
-  // toFillMeasure(duration: Duration = this.duration): number {
-  //   return (this.beatValue / duration.value) * (this.beats / 3);
-  // }
+  toFillMeasure(duration: Duration = this.duration): number {
+    return 0;
+    //return (this.beatValue / duration.value) * (this.beats / 3);
+  }
 }
 
 export interface RhythmicDuration {
