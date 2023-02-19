fe8d4243cfe15d5aace92659d71c717465c9374b
Pedro Santos <pedro.santos.personal@gmail.com>
2023-02-19 16:42:13 +0100

    refactor: note durations

--- src/Domain/Duration.ts
+++ src/Domain/Duration.ts
@@ -81,9 +81,32 @@ export type DurationPrimitives = {
   duration: number;
 };
 
+enum Durations {
+  Double = 2.0 / 1.0,
+  Whole = 1.0,
+  Half = 1.0 / 2.0,
+  Quarter = 1.0 / 4.0,
+  Eighth = 1.0 / 8.0,
+  Sixteenth = 1.0 / 16.0,
+  ThirtySecond = 1.0 / 32.0,
+  SixtyFourth = 1.0 / 64.0,
+}
+
+const ticksPerQuarterNote = 480;
+
+enum Ticks {
+  Double = ticksPerQuarterNote * 8,
+  Whole = ticksPerQuarterNote * 4,
+  Half = ticksPerQuarterNote * 2,
+  Quarter = ticksPerQuarterNote,
+  Eighth = ticksPerQuarterNote / 2,
+  Sixteenth = ticksPerQuarterNote / 4,
+  ThirtySecond = ticksPerQuarterNote / 8,
+  SixtyFourth = ticksPerQuarterNote / 16,
+}
+
 export class Duration {
   private static readonly all: Duration[] = [];
-  private static readonly ticksPerQuarterNote = 480;
   private static readonly dotMultiplier = 1.5;
   private static readonly doubleDotMultiplier = 1.75;
 
@@ -95,41 +118,29 @@ export class Duration {
     Duration.all.push(this);
   }
 
-  public static readonly Double: Duration = new Duration(
-    'Double',
-    2.0 / 1.0,
-    this.ticksPerQuarterNote * 8
-  );
-  public static readonly Whole: Duration = new Duration('Whole', 1.0, this.ticksPerQuarterNote * 4);
-  public static readonly Half: Duration = new Duration(
-    'Half',
-    1.0 / 2.0,
-    this.ticksPerQuarterNote * 2
-  );
+  public static readonly Double: Duration = new Duration('Double', Durations.Double, Ticks.Double);
+  public static readonly Whole: Duration = new Duration('Whole', Durations.Whole, Ticks.Whole);
+  public static readonly Half: Duration = new Duration('Half', Durations.Half, Ticks.Half);
   public static readonly Quarter: Duration = new Duration(
     'Quarter',
-    1.0 / 4.0,
-    this.ticksPerQuarterNote
+    Durations.Quarter,
+    Ticks.Quarter
   );
-  public static readonly Eighth: Duration = new Duration(
-    'Eighth',
-    1.0 / 8.0,
-    this.ticksPerQuarterNote / 2
-  );
+  public static readonly Eighth: Duration = new Duration('Eighth', Durations.Eighth, Ticks.Eighth);
   public static readonly Sixteenth: Duration = new Duration(
     'Sixteenth',
-    1.0 / 16.0,
-    this.ticksPerQuarterNote / 4
+    Durations.Sixteenth,
+    Ticks.Sixteenth
   );
   public static readonly ThirtySecond: Duration = new Duration(
     'ThirtySecond',
-    1.0 / 32.0,
-    this.ticksPerQuarterNote / 8
+    Durations.ThirtySecond,
+    Ticks.ThirtySecond
   );
   public static readonly SixtyFourth: Duration = new Duration(
     'SixtyFourth',
-    1.0 / 64.0,
-    this.ticksPerQuarterNote / 16
+    Durations.SixtyFourth,
+    Ticks.SixtyFourth
   );
   public static readonly DoubleDottedHalf: Duration = new Duration(
     'Double Dotted Half',
@@ -149,12 +160,12 @@ export class Duration {
   public static readonly DoubleDottedQuarter: Duration = new Duration(
     'Double Dotted Quarter',
     Duration.Quarter.duration * this.doubleDotMultiplier,
-    this.ticksPerQuarterNote * this.doubleDotMultiplier
+    ticksPerQuarterNote * this.doubleDotMultiplier
   );
   public static readonly DottedQuarter: Duration = new Duration(
     'Dotted Quarter',
     Duration.Quarter.duration * this.dotMultiplier,
-    this.ticksPerQuarterNote * this.dotMultiplier
+    ticksPerQuarterNote * this.dotMultiplier
   );
   public static readonly TripletHalf: Duration = new Duration(
     'Triplet Half',
@@ -189,7 +200,7 @@ export class Duration {
   public static readonly TripletEighth: Duration = new Duration(
     'Triplet Eighth',
     Duration.Quarter.duration / 3,
-    this.ticksPerQuarterNote / 3
+    ticksPerQuarterNote / 3
   );
   public static readonly DoubleDottedThirtySecond: Duration = new Duration(
     'Double Dotted ThirtySecond',
@@ -230,7 +241,7 @@ export class Duration {
   }
 
   static get tickPerQuarterNote() {
-    return Duration.ticksPerQuarterNote;
+    return ticksPerQuarterNote;
   }
 
   static From(value: number): Duration | undefined {
@@ -280,3 +291,108 @@ export class BeatsPerMinute {
     return (durationToBeats / this.bpm) * 60 * 1000;
   }
 }
+
+// Maybe a future option but not sure how this would work since
+// export const Duration2 = {
+//   Double: { name: 'Double', duration: Durations.Double, ticks: ticksPerQuarterNote * 8 },
+//   Whole: { name: 'Whole', duration: Durations.Whole, ticks: ticksPerQuarterNote * 4 },
+//   Half: { name: 'Half', duration: Durations.Half, ticks: ticksPerQuarterNote * 2 },
+//   Quarter: { name: 'Quarter', duration: Durations.Quarter, ticks: ticksPerQuarterNote },
+//   Eighth: { name: 'Eighth', duration: Durations.Eighth, ticks: ticksPerQuarterNote / 2 },
+//   Sixteenth: { name: 'Sixteenth', duration: Durations.Sixteenth, ticks: ticksPerQuarterNote / 4 },
+//   ThirtySecond: {
+//     name: 'ThirtySecond',
+//     duration: Durations.ThirtySecond,
+//     ticks: ticksPerQuarterNote / 8,
+//   },
+//   SixtyFourth: {
+//     name: 'SixtyFourth',
+//     duration: Durations.SixtyFourth,
+//     ticks: ticksPerQuarterNote / 16,
+//   },
+//   DoubleDottedHalf: {
+//     name: 'Double Dotted Half',
+//     duration: Durations.Half * doubleDotMultiplier,
+//     ticks: ticksPerQuarterNote * 2 * doubleDotMultiplier,
+//   },
+//   DottedHalf: {
+//     name: 'Dotted Half',
+//     duration: Durations.Half * dotMultiplier,
+//     ticks: Ticks.Half * dotMultiplier,
+//   },
+//   TripletWhole: {
+//     name: 'Triplet Whole',
+//     duration: Durations.Double / 3,
+//     ticks: Ticks.Double / 3,
+//   },
+//   DoubleDottedQuarter: {
+//     name: 'Double Dotted Quarter',
+//     duration: Durations.Quarter * doubleDotMultiplier,
+//     ticks: ticksPerQuarterNote * doubleDotMultiplier,
+//   },
+//   DottedQuarter: {
+//     name: 'Dotted Quarter',
+//     duration: Durations.Quarter * dotMultiplier,
+//     ticks: ticksPerQuarterNote * dotMultiplier,
+//   },
+//   TripletHalf: {
+//     name: 'Triplet Half',
+//     duration: Durations.Whole * 3,
+//     ticks: Ticks.Whole / 3,
+//   },
+//   DoubleDottedEighth: {
+//     name: 'Double Dotted Quarter',
+//     duration: Durations.Eighth * doubleDotMultiplier,
+//     ticks: Ticks.Eighth * doubleDotMultiplier,
+//   },
+//   DottedEighth: {
+//     name: 'Dotted Eight',
+//     duration: Durations.Eighth * dotMultiplier,
+//     ticks: Ticks.Eighth * dotMultiplier,
+//   },
+//   TripletQuarterNote: {
+//     name: 'Triplet Quarter',
+//     duration: Durations.Half / 3,
+//     ticks: Ticks.Half / 3,
+//   },
+//   DoubleDottedSixteenth: {
+//     name: 'Double Dotted Sixteenth',
+//     duration: Durations.Sixteenth * doubleDotMultiplier,
+//     ticks: Ticks.Sixteenth * doubleDotMultiplier,
+//   },
+//   DottedSixteenth: {
+//     name: 'Dotted Sixteenth',
+//     duration: Durations.Sixteenth * dotMultiplier,
+//     ticks: Ticks.Sixteenth * dotMultiplier,
+//   },
+//   TripletEighth: {
+//     name: 'Triplet Eighth',
+//     duration: Durations.Quarter / 3,
+//     ticks: ticksPerQuarterNote / 3,
+//   },
+//   DoubleDottedThirtySecond: {
+//     name: 'Double Dotted ThirtySecond',
+//     duration: Durations.ThirtySecond * doubleDotMultiplier,
+//     ticks: Ticks.ThirtySecond * doubleDotMultiplier,
+//   },
+//   DottedThirtySecond: {
+//     name: 'Dotted ThirtySecond',
+//     duration: Durations.ThirtySecond * dotMultiplier,
+//     ticks: Ticks.ThirtySecond * dotMultiplier,
+//   },
+//   TripletSixteenth: {
+//     name: 'Triplet Sixteenth',
+//     duration: Durations.Eighth / 3,
+//     ticks: Ticks.Eighth / 3,
+//   },
+//   DottedSixtyFourth: {
+//     name: 'Dotted SixtyFourth',
+//     duration: Durations.SixtyFourth * dotMultiplier,
+//     ticks: Ticks.SixtyFourth * dotMultiplier,
+//   },
+//   TripletThirtySecond: {
+//     name: 'Triplet ThirtySecond',
+//     duration: Durations.Sixteenth / 3,
+//     ticks: Ticks.Sixteenth / 3,
+//   },
+// };
