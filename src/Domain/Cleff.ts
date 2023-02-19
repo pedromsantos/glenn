96cfa8f6dacdbd81ae3b5d412ec992aa8cb10d9f
Pedro Santos <pedro.santos.personal@gmail.com>
2023-02-19 12:04:50 +0100

    chore:  brain dump possible cleff implementation

---
+++ src/Domain/Cleff.ts
@@ -0,0 +1,37 @@
+export const cleffBrainDump = 'cleffBrainDump';
+
+// import { Octave } from './Note';
+// import Pitch from './Pitch';
+
+// export enum ClefLine {
+//   First = 1,
+//   Second,
+//   Third,
+//   Fourth,
+//   Fifth,
+// }
+
+// export const ClefPitch = {
+//   G4: { pitch: Pitch.G, octave: Octave.C4 },
+//   C4: { pitch: Pitch.C, octave: Octave.C4 },
+//   F3: { pitch: Pitch.F, octave: Octave.C3 },
+// };
+
+// export enum ClefType {
+//   G,
+//   F,
+//   C,
+// }
+
+// export const Clef = {
+//   Treble: { clefPitch: ClefPitch.G4, line: ClefLine.Second, type: ClefType.G },
+//   FrenchViolin: { clefPitch: ClefPitch.G4, line: ClefLine.First, type: ClefType.G },
+//   BaritoneF: { clefPitch: ClefPitch.F3, line: ClefLine.Third, type: ClefType.F },
+//   BaritoneC: { clefPitch: ClefPitch.C4, line: ClefLine.Fifth, type: ClefType.C },
+//   Bass: { clefPitch: ClefPitch.F3, line: ClefLine.Fourth, type: ClefType.F },
+//   SubBass: { clefPitch: ClefPitch.F3, line: ClefLine.Fifth, type: ClefType.F },
+//   Alto: { clefPitch: ClefPitch.C4, line: ClefLine.Third, type: ClefType.C },
+//   Tenor: { clefPitch: ClefPitch.C4, line: ClefLine.Fourth, type: ClefType.C },
+//   Mezzo: { clefPitch: ClefPitch.C4, line: ClefLine.Second, type: ClefType.C },
+//   Soprano: { clefPitch: ClefPitch.C4, line: ClefLine.First, type: ClefType.C },
+// };
