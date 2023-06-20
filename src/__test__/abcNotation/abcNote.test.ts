e5a3152664819cd13a308717a77841fa34e416e9
Pedro Santos <pedro.santos.personal@gmail.com>
2023-06-20 14:05:22 +0200

    refactor: remove duplication in tests

--- src/__test__/abcNotation/abcNote.test.ts
+++ src/__test__/abcNotation/abcNote.test.ts
@@ -66,51 +66,25 @@ describe('abc Rest should', () => {
 
 describe('abc Note should', () => {
   describe('using octaves', () => {
-    test('represent a C4 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C4);
+    type TestTuple = [Octave, string];
+
+    test.each<TestTuple>([
+      [Octave.C2, 'C,,'],
+      [Octave.C3, 'C,'],
+      [Octave.C4, 'C'],
+      [Octave.C5, 'c'],
+      [Octave.C6, "C''"],
+      [Octave.C7, "C'''"],
+    ])('represent a note with octave', (octave: Octave, expected: string) => {
+      const note = new Note(Pitch.C, Duration.Whole, octave);
       const abc_note = new AbcNote(note, Duration.Whole);
 
-      expect(abc_note.toString()).toBe('C');
-    });
-
-    test('represent a C5 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C5);
-      const abc_note = new AbcNote(note, Duration.Whole);
-
-      expect(abc_note.toString()).toBe('c');
-    });
-
-    test('represent a C6 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C6);
-      const abc_note = new AbcNote(note, Duration.Whole);
-
-      expect(abc_note.toString()).toBe("C''");
-    });
-
-    test('represent a C7 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C7);
-      const abc_note = new AbcNote(note, Duration.Whole);
-
-      expect(abc_note.toString()).toBe("C'''");
-    });
-
-    test('represent a C3 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C3);
-      const abc_note = new AbcNote(note, Duration.Whole);
-
-      expect(abc_note.toString()).toBe('C,');
-    });
-
-    test('represent a C2 note', () => {
-      const note = new Note(Pitch.C, Duration.Whole, Octave.C2);
-      const abc_note = new AbcNote(note, Duration.Whole);
-
-      expect(abc_note.toString()).toBe('C,,');
+      expect(abc_note.toString()).toBe(expected);
     });
   });
 
   describe('using accidentals', () => {
-    test('represent a sharp note', () => {
+    test('represent a sharp F4 note', () => {
       const note = new Note(Pitch.FSharp, Duration.Whole, Octave.C4);
       const abc_note = new AbcNote(note, Duration.Whole);
 
@@ -118,13 +92,13 @@ describe('abc Note should', () => {
     });
 
     test('represent a sharp C5 note', () => {
-      const note = new Note(Pitch.FSharp, Duration.Whole, Octave.C5);
+      const note = new Note(Pitch.CSharp, Duration.Whole, Octave.C5);
       const abc_note = new AbcNote(note, Duration.Whole);
 
-      expect(abc_note.toString()).toBe('^f');
+      expect(abc_note.toString()).toBe('^c');
     });
 
-    test('represent a flat C5 note', () => {
+    test('represent a flat E5 note', () => {
       const note = new Note(Pitch.EFlat, Duration.Whole, Octave.C5);
       const abc_note = new AbcNote(note, Duration.Whole);
 
@@ -133,83 +107,37 @@ describe('abc Note should', () => {
   });
 
   describe('using durations', () => {
-    describe('relative to an eight note', () => {
-      test('represent a ThirtySecond note', () => {
-        const note = new Note(Pitch.EFlat, Duration.ThirtySecond, Octave.C3);
+    describe('relative to a sixteenth note', () => {
+      type TestTuple = [Octave, Duration, string];
+
+      test.each<TestTuple>([
+        [Octave.C3, Duration.ThirtySecond, '_E,/2'],
+        [Octave.C3, Duration.Sixteenth, '_E,'],
+        [Octave.C3, Duration.Eighth, '_E,2'],
+        [Octave.C4, Duration.Quarter, '_E4'],
+        [Octave.C5, Duration.Half, '_e8'],
+      ])('represent a note', (octave: Octave, duration: Duration, expected: string) => {
+        const note = new Note(Pitch.EFlat, duration, octave);
         const abc_note = new AbcNote(note, Duration.Sixteenth);
 
-        expect(abc_note.toString()).toBe('_E,/2');
-      });
-
-      test('represent a Sixteenth note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Sixteenth, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Sixteenth);
-
-        expect(abc_note.toString()).toBe('_E,');
-      });
-
-      test('represent a eight note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Eighth, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Sixteenth);
-
-        expect(abc_note.toString()).toBe('_E,2');
-      });
-
-      test('represent a quarter note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Quarter, Octave.C4);
-        const abc_note = new AbcNote(note, Duration.Sixteenth);
-
-        expect(abc_note.toString()).toBe('_E4');
-      });
-
-      test('represent a half note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Half, Octave.C5);
-        const abc_note = new AbcNote(note, Duration.Sixteenth);
-
-        expect(abc_note.toString()).toBe('_e8');
+        expect(abc_note.toString()).toBe(expected);
       });
     });
-    describe('relative to an whole note', () => {
-      test('represent a Sixteenth note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Sixteenth, Octave.C3);
+
+    describe('relative to a whole note', () => {
+      type TestTuple = [Octave, Duration, string];
+
+      test.each<TestTuple>([
+        [Octave.C3, Duration.Sixteenth, '_E,/16'],
+        [Octave.C3, Duration.Eighth, '_E,/8'],
+        [Octave.C3, Duration.Quarter, '_E,/4'],
+        [Octave.C3, Duration.Half, '_E,/2'],
+        [Octave.C3, Duration.Whole, '_E,'],
+      ])('represent a note', (octave: Octave, duration: Duration, expected: string) => {
+        const note = new Note(Pitch.EFlat, duration, octave);
         const abc_note = new AbcNote(note, Duration.Whole);
 
-        expect(abc_note.toString()).toBe('_E,/16');
-      });
-
-      test('represent a Eighth note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Eighth, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Whole);
-
-        expect(abc_note.toString()).toBe('_E,/8');
-      });
-
-      test('represent a Quarter note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Quarter, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Whole);
-
-        expect(abc_note.toString()).toBe('_E,/4');
-      });
-
-      test('represent a Half note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Half, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Whole);
-
-        expect(abc_note.toString()).toBe('_E,/2');
-      });
-
-      test('represent a Whole note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Whole, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Whole);
-
-        expect(abc_note.toString()).toBe('_E,');
-      });
-
-      test('represent a Double note', () => {
-        const note = new Note(Pitch.EFlat, Duration.Double, Octave.C3);
-        const abc_note = new AbcNote(note, Duration.Whole);
-
-        expect(abc_note.toString()).toBe('_E,2');
+        expect(abc_note.toString()).toBe(expected);
       });
     });
   });
