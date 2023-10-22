39854b254259eeed6a731f9e022c588ca2deb0fa
Pedro Santos <pedro.santos.personal@gmail.com>
2023-10-22 16:48:20 +0200

    feat: add duration to abc chord

--- src/__test__/abcNotation/abcChord.test.ts
+++ src/__test__/abcNotation/abcChord.test.ts
@@ -1,18 +1,19 @@
 import { AbcChord, AbcGuitarChord } from '../../abcNotation/abcChord';
 import { ChordPattern, ClosedChord } from '../../Domain/Chord';
+import { Duration } from '../../Domain/Duration';
 import { Pitch } from '../../Domain/Pitch';
 
 describe('abc Chord should', () => {
   test('convert a C major triad to abc notation', () => {
     const chord = new ClosedChord(Pitch.C, ChordPattern.Major);
-    const abc_chord = new AbcChord(chord);
+    const abc_chord = new AbcChord(chord, Duration.Whole);
 
     expect(abc_chord.toString()).toBe('[CEG]');
   });
 
   test('convert a C major 7 to abc notation', () => {
     const chord = new ClosedChord(Pitch.C, ChordPattern.Major7);
-    const abc_chord = new AbcChord(chord);
+    const abc_chord = new AbcChord(chord, Duration.Whole);
 
     expect(abc_chord.toString()).toBe('[CEGB]');
   });
--- src/abcNotation/abcChord.ts
+++ src/abcNotation/abcChord.ts
@@ -1,11 +1,19 @@
+import { Duration } from 'src/Domain/Duration';
+
 import { Chord } from '../Domain/Chord';
-import { AbcPitch } from './abcNote';
+import { AbcNote } from './abcNote';
 
 export class AbcChord {
-  constructor(private readonly chord: Chord) {}
+  constructor(
+    private readonly chord: Chord,
+    private readonly defaultDuration: Duration
+  ) {}
 
   toString() {
-    return `[${[...this.chord].map((p) => new AbcPitch(p).toString()).join('')}]`;
+    const chordNotes = this.chord.Notes;
+    const abcNotes = [...chordNotes].map((n) => new AbcNote(n, this.defaultDuration));
+
+    return `[${abcNotes.map((n) => n.toString()).join('')}]`;
   }
 }
 
